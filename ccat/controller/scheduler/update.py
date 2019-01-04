7b03a7aafabd45b1c483c1d690bfc974b210decf
bliiir <rasgroth@gmail.com>
2019-01-04 17:28:53 +0100

    Added the update script to retrieve candles on a schedule run by crontab on the server

---
+++ ccat/controller/scheduler/update.py
@@ -0,0 +1,76 @@
+'''
+------------------------------------------------------------------------
+    UPDATE.PY
+------------------------------------------------------------------------
+Updates the database with exchange data for a given market and timeframe
+'''
+
+
+'''
+------------------------------------------------------------------------
+    IMPORTS
+------------------------------------------------------------------------
+'''
+
+# Standard library imports
+pass
+
+# Third party imports
+pass
+
+# Local application imports
+from ccat import config as cf
+from ccat import bucket
+
+
+'''
+------------------------------------------------------------------------
+    FUNCTIONS
+------------------------------------------------------------------------
+'''
+
+def update(
+    market_id = 1, # Bitmex
+    timeframe_id = 6,  # 1d
+    time_end = cf.now(),
+    count = 500):
+
+    bucket.Bucket(
+        market_id=market_id,
+        timeframe_id=timeframe_id).update(count=count, time_end=time_end)
+
+'''
+------------------------------------------------------------------------
+    __MAIN__
+------------------------------------------------------------------------
+'''
+
+if __name__ == '__main__':
+
+    import sys
+    import time
+    import logging
+    from datetime import datetime as dt
+
+    # Get arguments from the cronjob
+    market_id = int(sys.argv[1])
+    timeframe_id = int(sys.argv[2])
+    count = int(sys.argv[3])
+
+    # Do the update
+    update(market_id = market_id, timeframe_id = timeframe_id, count=count)
+
+    # Log the event
+    logging.basicConfig(filename=cf.lf_bucket_update, level=logging.DEBUG) # /home/ubuntu/logs/bucket_update.log
+    # logging.info(f'{cf.db_host}, {cf.db_name}, updated @ {dt.strftime(dt.now(), "%Y.%M.%d %H:%M:%S")}')
+
+    # Set the current time for the log
+    time_now = dt.strftime(dt.now(), "%Y.%M.%d %H:%M:%S")
+
+    logging.info(f'\
+        host: {cf.db_host}, \
+        db: {cf.db_name}, \
+        market_id: {market_id}, \
+        timeframe_id: {timeframe_id}, \
+        count: {count}, \
+        time_now: {time_now}')
