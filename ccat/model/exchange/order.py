fb7de6d4d574b2bc7725ed114365280c0c568fc1
bliiir <rasgroth@gmail.com>
2019-01-23 14:36:34 +0100

    Finished the order module including the unit-tests

--- ccat/model/exchange/order.py
+++ ccat/model/exchange/order.py
@@ -35,14 +35,56 @@ class Order():
     '''Generalized order class to interact with the exchanges
     '''
 
-    def __init__(self):
+    def __init__(self, market_id=1):
         '''Set internal id
         '''
-        self.internal_id = uuid.uuid4()
-        self.timestamp_created = config.now()
+        # Set internal id and timestamp
+        # self.internal_id = uuid.uuid4()
+        # self.timestamp_created = config.now()
 
+        self.market_id = market_id
+
+        # Get the exchange_id and symbol_ccxt from the market_id
+        self.market = Market(self.market_id).get()
+        self.exchange_id = self.market.exchange_id
+        self.symbol_ccxt =self.market.symbol_ccxt
+        self.symbol_native =self.market.symbol_native
+
+        # Get exchange client from market_id
+        self.exchange_client = Exchange(exchange_id=self.exchange_id).client()
+
+        self.order_id = None
+        self.order_status = None
+        self.order_price = None
+        self.order_time_executed = None
+        self.order_time_submitted = None
+
+
+    # Set standard output
     def __str__(self):
-        pass
+
+
+                # "internal id":self.internal_id,
+                # "internal timestamp":self.timestamp_created,
+
+        try:
+            out = {
+                "market_id":self.market_id,
+                "exchange_id":self.exchange_id,
+                "symbol_native":self.symbol_native,
+                "order_id":self.order_id,
+                "order_status":self.order_status,
+                "order_time_submitted":self.order_time_submitted,
+                "order_time_executed":self.order_time_executed,
+                "order_price":self.order_price,
+                "symbol_ccxt":self.symbol_ccxt}
+
+            return str(out)
+
+        except Exception as e:
+            error_message = 'Error: ' + str(e)
+            return error_message
+
 
     # Set post-order-creation instance attributes
     def populate(self):
@@ -50,11 +92,9 @@ class Order():
         self.order_status = self.order['info']['ordStatus']
         self.order_price = self.order['info']['price']
         self.order_time_executed = self.order['info']['transactTime']
-        self.order_time_subimtted = self.order['info']['timestamp']
+        self.order_time_submitted = self.order['info']['timestamp']
 
-
-    # def get_symbol_ccxt(self):
-    #     pass
+        return self
 
 
     # Load order from exchange by order_id or self.order_id
@@ -69,71 +109,51 @@ class Order():
         # Update the object with the new info from the exchange
         self.populate()
 
+        return self
 
-    def submit(
-        self,
-        side,
-        amount,
-        order_type='market',
-        market_id=1):
-        '''Submit a new order to the exchange
+
+    def submit(self, side, amount, order_type = 'market'):
+        '''Submit a new order to the exchange.
+
+        The default order type is a market order
         '''
 
         # Set pre-order-creation instance attributes
         self.order_side = side
         self.order_amount = amount,
         self.order_type = order_type
-        self.market_id = market_id
 
-        # Get the exchange_id from the market_id
-        self.market = Market(self.market_id).get()
-        self.exchange_id = self.market.exchange_id
-        self.symbol_ccxt =self.market.symbol_ccxt
-        self.symbol_native =self.market.symbol_native
-
-        # Get exchange client from market_id
-        self.exchange = Exchange(exchange_id=self.exchange_id)
-        self.exchange_client = self.exchange.client()
-
-        # Place and load the order
+        # Place and load the order into self.order
         self.order = self.exchange_client.create_order(
             side=self.order_side,
             symbol=self.symbol_ccxt,
             type=self.order_type,
-            amount=self.order_amount,
-            )
+            amount=self.order_amount)
 
+        # Populate the instance attributes from self.order
         self.populate()
 
         return self
 
 
-    def delete(self, order_id=None):
+    def cancel(self, order_id=None):
         '''Delete unfilled orders
-
-        Check the order status - if it does not say 'open', then return
-        an error message with the status.
-
-        Possible responses: 'open', 'closed', 'cancelled', 'rejected', 'filled'.
-        See more detail under parse_order_status here:
-        https://github.com/ccxt/ccxt/blob/master/python/ccxt/bitmex.py
         '''
 
         if order_id != None:
             self.order_id = order_id
 
-        # Load the order from the exchange
-        self.load()
+        try:
 
-        # Return the order status if the order status on the exchange
-        # isn't 'open'
-        if self.order_status != open:
-            return self.order_status
-
-        else:
+            # Cancel the order
             self.order = self.exchange_client.cancel_order(id=self.order_id)
-            self.load()
-            return self.order_status
+
+            # return the status
+            return self.order['info']['ordStatus']
+
+        except Exception as e:
+            message = "ERROR: " + str(e)
+            return message
 
 
 '''
@@ -148,9 +168,12 @@ class Test_order(unittest.TestCase):
 
     # Setup
     def setUp(self):
-        self.my_order = Order()
 
-        self.status_options =  [
+        self.my_order = Order(market_id=1)
+
+        self.test_id = '8a26a525-c6e6-f284-754c-7ebcf221ac7a'
+
+        self.status_options = [
             'canceled',
             'open',
             'closed',
@@ -167,62 +190,57 @@ class Test_order(unittest.TestCase):
             'untriggered',
             'triggered']
 
-    # def test_order_not_none(self):
-    #     print("\nORDER ID: ", self.my_order.internal_id)
-    #     self.assertIsNotNone(self.my_order)
 
-    # def test_order_id(self):
-    #     print('\nLENGTH: ', len(str(self.my_order.internal_id)))
-    #     self.assertEqual(len(str(self.my_order.internal_id)), 36)
-
-    # def test_load_from_id(self):
-    #     pass
-
-    # def test_submit(self):
-    #     order = self.my_order.submit(
-    #         side = 'sell',
-    #         amount = 1,
-    #         order_type ='market',
-    #         market_id = 1)
-    #     print('STATUS: ', order.order_status)
-    #     print('ORDER ID: ', order.order_id)
-    #     self.assertIn(order.order_status.lower(), self.status_options)
+    def test_str_(self):
+        self.assertIn("order_status", self.my_order.__str__())
 
 
-    def test_load(self):
-        order = self.my_order.load(order_id = '8a26a525-c6e6-f284-754c-7ebcf221ac7a')
+    def test_order_not_none(self):
+        # print("\nORDER ID: ", self.my_order.internal_id)
+        self.assertIsNotNone(self.my_order)
+
+
+    def test_load_order_from_id(self):
+        test_id = '8a26a525-c6e6-f284-754c-7ebcf221ac7a'
+        test_order = self.my_order.load(order_id = test_id)
+        print('\nLOAD ORDER FROM ID TEST: ', test_order)
+        print('\nLOAD ORDER FROM ID STATUS TEST: ', test_order.order_status.lower())
+        self.assertIn(test_order.order_status.lower(), self.status_options)
+
+
+    def test_load_order_from_self(self):
+
+        # Load order from exchange based on known order id
+        # to avoid having to use funds
+        test_order1 = self.my_order.load(order_id = self.test_id)
+
+        # Load order from exchange based on self.order_id
+        test_order2 = test_order1.load()
+
+        print('\nLOAD ORDER TEST: ', test_order2)
+        print('\nLOAD ORDER STATUS TEST: ', test_order2.order_status.lower())
+        self.assertIn(test_order2.order_status.lower(), self.status_options)
+
+
+    # CAREFUL - this submits a real market order to the exchange
+    def test_submit(self):
+        order = self.my_order.submit(side = 'sell', amount = 1)
         print('STATUS: ', order.order_status)
         print('ORDER ID: ', order.order_id)
         self.assertIn(order.order_status.lower(), self.status_options)
 
 
-    # # Ways of making an order instance
-    # def test_submit_new_order_to_exchange(self):
+    # CAREFUL - this submits a real market order to the exchange
+    def test_cancel_order_via_self(self):
+        order = self.my_order.submit(side = 'sell', amount = 1)
+        status = order.cancel()
+        print("\nCANCEL STATUS, SELF: ", status)
 
-    #     self.my_order.submit(
-    #         side='sell',
-    #         amount=1,
-    #         order_type='market',
-    #         market_id=1)
 
-    #     self.assertIsNotNone(self.my_order)
-
-    # def test_load_existing_order_from_exchange_via_self(self):
-    #     pass
-
-    # # Ways of getting an order's status
-    # def test_load_existing_order_from_exchange_via_order_id(self):
-    #     pass
-
-    # def test_update_existing_order_status_from_order_id(self):
-    #     pass
-
-    # def test_delete_order_via_self(self):
-    #     pass
-
-    # def test_delete_order_via_order_id(self):
-    #     pass
-
+    def test_cancel_order_via_order_id(self):
+        order = self.my_order.load(order_id = self.test_id)
+        status = order.cancel()
+        print("\nCANCEL STATUS, ORDER ID: ", status)
 
 
 '''
