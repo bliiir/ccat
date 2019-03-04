c91266a37c5c481abfa70e3097de3e895afc297c
bliiir <rasgroth@gmail.com>
2019-03-04 13:49:21 +0100

    Moved local unittests to ccat/test and tweaked the indicators input parameters and the format of the returned dataframes

--- ccat/controller/feature/height.py
+++ ccat/controller/feature/height.py
@@ -5,7 +5,6 @@
 Calculates the heights of the different parts of the candles
 '''
 
-
 '''
 ------------------------------------------------------------------------
     IMPORTS
@@ -20,7 +19,7 @@ import pandas as pd
 import numpy as np
 
 # Local application imports
-pass
+from ccat import config
 
 '''
 ------------------------------------------------------------------------
@@ -38,8 +37,8 @@ pass
 
 def get(df_in: pd.DataFrame) -> pd.DataFrame:
 
-    '''Calculates the height of the candle and its elements - total,
-    body top-wick and bottom-wick
+    '''Calculates the absolute (abs) and relative (pct) height of the
+    candle and its elements - total, body, top-wick and bottom-wick
 
     Args:
         :df_in (pandas dataframe):  Dataframe with (at least) columns:
@@ -53,28 +52,34 @@ def get(df_in: pd.DataFrame) -> pd.DataFrame:
         :df_out (pandas dataframe): Dataframe with (exactly) columns:
 
             height_total:
-                Difference between the price_high and
+                Absolute difference between the price_high and
                 price_low denominated in the quota asset,
 
             height_body:
-                Difference between the price_close and
+                Absolute difference between the price_close and
                 price_open denominated in the quota asset,
 
             height_top:
-                Difference between the price_close (if higher than
+                Absolute difference between the price_close (if higher than
                 price_open, and price_open if not) and price_high
                 denominated in the quota asset,
 
             height_bottom:
-                Difference between the price_close (if lower than
+                Absolute difference between the price_close (if lower than
                 price_open, and price_open if not) and price_high
                 denominated in the quota asset,
 
+            percent_body:
+                Relative height of the body to the total
+                height_body/height_total
+
             percent_top:
-                height_total/height_top,
+                Relative height of the top wick to the total
+                height_top/height_total,
 
             percent_bottom:
-                height_total/height_bottom
+                Relative height of the bottom wick to the total
+                height_bottom/height_total
     '''
 
     df_out = pd.DataFrame()
@@ -86,58 +91,29 @@ def get(df_in: pd.DataFrame) -> pd.DataFrame:
     df_out.set_index('id')
 
     # Total height
-    df_out['height_total']=df_in.price_high-df_in.price_low
+    df_out['abs_total'] = df_in.price_high-df_in.price_low
 
     # Body height
-    df_out['height_body']=df_in.price_close-df_in.price_open
+    df_out['abs_body'] = df_in.price_close-df_in.price_open
 
     # Top wick height
-    df_out['height_top']=np.where(
-                                df_in.price_close >= df_in.price_open,
-                                df_in.price_high - df_in.price_close,
-                                df_in.price_high - df_in.price_open)
+    df_out['abs_top']=np.where(
+        df_in.price_close >= df_in.price_open,
+        df_in.price_high - df_in.price_close,
+        df_in.price_high - df_in.price_open)
 
     # Bottom wick height
-    df_out['height_bottom']=np.where(
-                                df_in.price_close < df_in.price_open,
-                                df_in.price_close - df_in.price_low ,
-                                df_in.price_open - df_in.price_low)
+    df_out['abs_bottom']=np.where(
+        df_in.price_close < df_in.price_open,
+        df_in.price_close - df_in.price_low ,
+        df_in.price_open - df_in.price_low)
+
+    df_out['pct_body'] = df_out['abs_body'] / df_out['abs_total']
 
     # Top Wick height percentage of total candle height
-    df_out['percent_top']=(
-                                df_out['height_top'] /
-                                df_out['height_total'])
+    df_out['pct_top'] = df_out['abs_top'] / df_out['abs_total']
 
     # Bottom Wick height percentage of total candle height
-    df_out['percent_bottom'] = (
-                                df_out['height_bottom'] /
-                                df_out['height_total'])
+    df_out['pct_bottom'] = df_out['abs_bottom'] / df_out['abs_total']
 
     return df_out
-
-
-'''
-------------------------------------------------------------------------
-    __MAIN__
-------------------------------------------------------------------------
-'''
-
-if __name__ == '__main__':
-
-    from ccat import bucket
-
-    # Get a bucket object from Bucket
-    b = bucket.Bucket(market_id=1,timeframe_id=1)
-
-    # Update the table
-    b.update()
-
-    # Get a dataframe with all the data for the market and timeframe
-    df_in = b.read_all()
-
-    # Calculate the heights
-    h = get(df_in)
-    print(h)
-
-
-
