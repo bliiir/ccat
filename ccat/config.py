1034553c9f684e94e3efe2451925c8251198f085
bliiir <rasgroth@gmail.com>
2019-03-08 16:56:37 +0100

    Split config up so all the time stuff is now in ccat.controller.helper.time.py

--- ccat/config.py
+++ ccat/config.py
@@ -6,13 +6,16 @@
 
 # Standard library imports
 import os
-import time
-import datetime
-from math import floor
+
+# Third party packages
+pass
+
+# Local packages
+pass
 
 '''
 ------------------------------------------------------------------------- - - -
-    CONFIG
+    ENVIRONMENT
 ------------------------------------------------------------------------- - - -
 '''
 
@@ -27,77 +30,3 @@ db_password = os.environ['DB_PW']
 # log files
 lf_bucket_update = os.environ["LF_BUCKET_UPDATE"]
 lf_bucket_historical = os.environ["LF_BUCKET_HISTORICAL"]
-
-
-# Time
-def now():
-    return floor((datetime.datetime.now().timestamp())*1000)
-
-def hour_ago():
-    return now()-3600000
-
-def day_ago():
-    return now()-86400000
-
-def week_ago():
-    return now()-604800000
-
-def month_ago():
-    return now()-2592000000
-
-
-'''
-------------------------------------------------------------------------
-    UNITTEST
-------------------------------------------------------------------------
-'''
-
-import unittest
-
-class Test_market(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def test_environment_variables(self):
-        pass
-
-    def test_now(self):
-        self.assertIsInstance(now(), int)
-
-    def test_hour_ago(self):
-        self.assertIsInstance(hour_ago(), int)
-
-    def test_day_ago(self):
-        self.assertIsInstance(day_ago(), int)
-
-    def test_week_ago(self):
-        self.assertIsInstance(week_ago(), int)
-
-    def test_month_ago(self):
-        self.assertIsInstance(month_ago(), int)
-
-
-
-'''
-------------------------------------------------------------------------
-    MAIN
-------------------------------------------------------------------------
-'''
-
-if __name__ == '__main__':
-
-    unittest.main()
-
-'''
-------------------------------------------------------------------------- - - -
-    MAIN
-------------------------------------------------------------------------- - - -
-'''
-
-# Run if executed directly. Do not run it if import
-if __name__ == '__main__':
-    print('datetime.datetime: \t', now())
-    print('time.time: \t\t', time.time())
-    print('your tz: \t\t', datetime.datetime.fromtimestamp(now()/1000))
-    print('utc: \t\t\t', datetime.datetime.utcfromtimestamp(now()/1000))
